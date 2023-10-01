6387487431d3cc803a39f8432caa56cdb8a5a6d0
raybackk <raiback14hc@gmail.com>
2023-10-01 15:33:57 -0600

    Actualizacion_Archivos

---
+++ src/main/kotlin/Operadores.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val x1=4; val x2=-3; val y1=3; val y2=-2
+    val m=(y2-y1)/(x2-x1)
+    print("El calculo de la pendiente es: $m")
+}
---
+++ src/main/kotlin/Variables_Tipos_Datos.kt
@@ -0,0 +1,9 @@
+const val PI=3.1416
+fun main() {
+    val floatDef=4.21
+    println("Variable flotante por defecto $floatDef")
+    val flotante:Float=3.21f
+    println("Variable decimal convertida a flotante: $flotante")
+    val resFloat=PI*(flotante*flotante)
+    println("El perimetro del criculo  es: $resFloat centimetros cuadrados")
+}
